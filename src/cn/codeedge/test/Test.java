02364a0d57f8de297f9d122377542ec6bd730da4
程琼琼 <645277836@qq.com>
2020-03-13 16:09:53 +0800

    test

--- src/cn/codeedge/test/Test.java
+++ src/cn/codeedge/test/Test.java
@@ -2,6 +2,6 @@ package cn.codeedge.test;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("hello啊 树先生！");
     }
 }
